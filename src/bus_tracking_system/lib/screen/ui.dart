a576cf971424fb3f2651a0de7c2327217d1d9ccc
prathamkandari <prathamkandari123@gmail.com>
2023-07-12 00:17:22 +0530

    Pipeline

---
+++ src/bus_tracking_system/lib/screen/ui.dart
@@ -0,0 +1,225 @@
+import 'dart:math';
+import 'package:bus_tracking_system/Constants/constants.dart';
+import 'package:bus_tracking_system/helper/helperFunction.dart';
+import 'package:bus_tracking_system/screen/splash.dart';
+import 'package:bus_tracking_system/services/authServices.dart';
+import 'package:bus_tracking_system/services/databaseServices.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart'; 
+import 'package:bus_tracking_system/screen/locations_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+// import 'package:location/location.dart';
+
+class UI extends StatefulWidget {
+  //is a
+  @override
+  State<UI> createState() => _UIState();
+}
+
+class _UIState extends State<UI> {
+  AuthService _auth = AuthService();
+  bool isStudent = true;
+  late final String email;
+  late final String password;
+  final _formfield = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+  bool passToggle = true;
+
+  late DatabaseReference dbRef;
+
+  void toggleLoginOption() {
+    setState(() {
+      isStudent = !isStudent;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    dbRef = FirebaseDatabase.instance.ref().child('Students');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'BUS TRACKER',
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 60),
+          child: Form(
+              key: _formfield,
+              child: SingleChildScrollView(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    isStudent ? 'STUDENT LOGIN' : 'DRIVER LOGIN',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.email),
+                    ),
+                    validator: (value) {
+                      bool emailValid = RegExp(
+                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(value!);
+
+                      if (value.isEmpty) {
+                        return "Enter Email";
+                      } else if (!emailValid) {
+                        return "Enter valid Email";
+                      }
+                    },
+                  ),
+                  // fireba(context, "Sign In", () {
+                  //   FirebaseAuth.instance
+                  //       .signInWithEmailAndPassword(
+                  //           email: emailController.text,
+                  //           password: passController.text)
+                  //       .then((value) {
+                  //     Navigator.push(context,
+                  //         MaterialPageRoute(builder: (context) => LocationsPage()));
+                  //   }).onError((error, stackTrace) {
+                  //     print("Error ${error.toString()}");
+                  //   });
+                  // }),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: passController,
+                    obscureText: passToggle,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: InkWell(
+                          onTap: () {
+                            setState(() {
+                              passToggle = !passToggle;
+                            });
+                          },
+                          child: Icon(passToggle
+                              ? Icons.visibility_off
+                              : Icons.visibility)),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Enter Password';
+                      } else if (passController.text.length < 9) {
+                        return "Password length should be more than 9 characters";
+                      }
+                    },
+                  ),
+                  SizedBox(height: 20),
+
+                  InkWell(
+                    onTap: () {
+                      Map<String, String> Students = {
+                        'email': emailController.text,
+                        'password': passController.text,
+                      };
+
+                      dbRef.push().set(Students);
+
+                      FirebaseAuth.instance
+                          .signInWithEmailAndPassword(
+                              email: emailController.text,
+                              password: passController.text)
+                          .then((value) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (BuildContext context) {
+                            return LocationsPage();
+                          }),
+                        );
+                      });
+
+                      // if (_formfield.currentState!.validate()) {
+                      //   print("Login Successful");
+                      //   emailController.clear();
+                      //   passController.clear();
+                      //   Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //       builder: (BuildContext context) {
+                      //         return LocationsPage();
+                      //       },
+                      //     ),
+                      //   );
+                      // }
+                    },
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 10),
+                  TextButton(
+                    onPressed: toggleLoginOption,
+                    child: Text(
+                      isStudent ? 'Log in as Driver' : 'Log in as Student',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ],
+              ))),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(StringProperty('password', password));
+  }
+
+//   login() async {
+//     await _auth
+//         .loginWithUserEmailandPassword(email, password)
+//         .then((value) async {
+//       //auth services instance
+//       if (value == true) {
+//         QuerySnapshot snapshot =
+//             await DatabaseService(uid: FirebaseAuth.instance.currentUser!.uid)
+//                 .getUserData(email);
+//         //sharedrefences
+//         await HelperFunctions.savedUserLoggedInStatus(true);
+//         await HelperFunctions.savedUserEmailSF(email);
+//       }
+//     });
+//   }
+}
